192db5c851eadbbfc46e1e99af6cf3010cf5c422
WillKoehrsen <wjk68@case.edu>
2018-10-23 15:46:03 +0000

    Working on editing notebooks

--- churn/best_pipeline.py
+++ churn/best_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Binarizer
+from sklearn.preprocessing import MinMaxScaler
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.19956765479311114
+# Average CV score on the training set was:0.43811110082970917
 exported_pipeline = make_pipeline(
-    Binarizer(threshold=1.0),
-    BernoulliNB(alpha=100.0, fit_prior=False)
+    MinMaxScaler(),
+    GradientBoostingClassifier(learning_rate=0.01, max_depth=7, max_features=0.7500000000000001, min_samples_leaf=12, min_samples_split=3, n_estimators=100, subsample=0.05)
 )
 
 exported_pipeline.fit(training_features, training_target)
