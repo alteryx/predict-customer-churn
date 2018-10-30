a78cf99283c8d1e4c857851680f9ea92e806f15b
Will Koehrsen <wjk68@case.edu>
2018-10-30 19:05:28 +0000

    Cleaning up

--- churn/best_pipeline_new.py
+++ churn/best_pipeline_new.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.naive_bayes import BernoulliNB, GaussianNB
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,14 +12,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=50)
 
-exported_pipeline = ExtraTreesClassifier(bootstrap=False, 
-                                         criterion="entropy", 
-                                         max_features=0.1, 
-                                         min_samples_leaf=5, 
-                                         min_samples_split=8, 
-                                         n_estimators=100,
-                                         random_state = 50,
-                                         n_jobs = -1)
+# Average CV score on the training set was:0.08620222304609472
+exported_pipeline = make_pipeline(
+    StandardScaler(),
+    StackingEstimator(estimator=BernoulliNB(alpha=1.0, fit_prior=True)),
+    GaussianNB()
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
