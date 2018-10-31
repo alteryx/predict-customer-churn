32ac2b4bf43a564813dc1dcbb44805e34e55b58a
Will Koehrsen <wjk68@case.edu>
2018-10-31 16:41:48 +0000

    Running labeling again

--- churn/best_pipeline_new.py
+++ churn/best_pipeline_new.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import BernoulliNB, GaussianNB
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import StandardScaler
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.tree import DecisionTreeClassifier
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,11 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=50)
 
-# Average CV score on the training set was:0.08620222304609472
+# Average CV score on the training set was:0.7287682151944667
 exported_pipeline = make_pipeline(
-    StandardScaler(),
-    StackingEstimator(estimator=BernoulliNB(alpha=1.0, fit_prior=True)),
-    GaussianNB()
+    SelectPercentile(score_func=f_classif, percentile=97),
+    DecisionTreeClassifier(criterion="entropy", max_depth=7, min_samples_leaf=7, min_samples_split=3)
 )
 
 exported_pipeline.fit(training_features, training_target)
